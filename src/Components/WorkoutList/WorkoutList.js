116273c75ff8e47b32ec17b8244d12adbf2c0b37
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-12-30 15:27:52 -0500

    able to fetch data from database using client. next display results to browser

---
+++ src/Components/WorkoutList/WorkoutList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Workouts extends Component {
+static defaultProps = {
+    workouts: []
+}
+
+    
+    render() {
+        return (
+            <div>
+                <h2>Your Workouts</h2>
+
+            </div>
+        )
+    }
+}
+
+export default Workouts
--- src/Components/WorkoutPage/workoutPage.js
+++ src/Components/WorkoutPage/workoutPage.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
 import Header from '../Header/Header.js'
+import AddWorkoutForm from '../AddWorkoutForm/AddWorkoutForm.js'
+import Workouts from '../WorkoutList/WorkoutList'
+import config from '../../config';
 
 
 class WorkoutPage extends Component {
+  state = {
+      workouts: [],
+      error: null,
+  }
 
+  setWorkouts = workouts => {
+      this.setState({
+          workouts,
+          error: null,
+      })
+      console.log(this.state.workouts)
+  }
 
-
+componentDidMount(){
+    fetch(config.API_ENDPOINT, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    .then(res => {
+        if(!res.ok){
+            throw new Error(res.status)
+        }
+        console.log(res)
+        return res.json()
+    })
+    .then(this.setWorkouts)
+    .catch(error => this.setState({error}))
+}
 
 
     render() {
         return (
             <div>
                 <Header />
-                {/*<Route path={'/'} />*/}
-                {/* <Workouts /> */}
+                <main>
+                <Link to="/AddWorkoutForm">Add Workout</Link>
+                <Route path={'/AddWorkoutForm'} component={AddWorkoutForm} />
+                <Workouts workouts={this.state.bookmarks}/>
+                </main>
             </div>
         )
     }
