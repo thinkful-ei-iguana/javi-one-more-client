bcc8fb40149ee39934fee62fd332fe71b505f00a
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-12-31 09:50:51 -0500

    able to edit workout and update. next delete button

--- src/Components/EditWorkout/EditWorkout.js
+++ src/Components/EditWorkout/EditWorkout.js
@@ -49,28 +49,50 @@ export class EditWorkout extends Component {
       };
     
       handleChangeWorkout1 = e => {
-        this.setState({ url: e.target.value })
+        this.setState({ workout1: e.target.value })
       };
     
       handleChangelbs = e => {
-        this.setState({ description: e.target.value })
+        this.setState({ lbs: e.target.value })
       };
     
       handleChangeSet1 = e => {
-        this.setState({ rating: e.target.value })
+        this.setState({ set1: e.target.value })
       };
 
       handleChangeSet2 = e => {
-        this.setState({ rating: e.target.value })
+        this.setState({ set2: e.target.value })
       };
 
       handleChangeSet3 = e => {
-        this.setState({ rating: e.target.value })
+        this.setState({ set3: e.target.value })
       };
 
-handleClickCancel = () => {
+      handleClickCancel = () => {
     this.props.history.push('/workoutPage')
-}
+      };
+
+      handleSubmit = e => {
+          e.preventDefault()
+
+          const { id } = this.props.match.params
+          const { title, workout1, lbs, set1, set2, set3 } = this.state
+          const newWorkout = {title, workout1, lbs, set1, set2, set3}
+
+          fetch(config.API_ENDPOINT + `/${id}`, {
+            method: 'PATCH',
+            body: JSON.stringify(newWorkout),
+            headers: {
+                'content-type': 'application/json',
+             }
+            })
+            .then(() => {
+                this.props.history.push('/workoutPage')
+            })
+            .catch(error => {
+                console.log(error)
+            })
+      }
 
 
     render() {
@@ -86,28 +108,29 @@ handleClickCancel = () => {
 
                           <div>
                             <label htmlFor="title">Title:</label>
-                            <input id="title" name="title"type="text" placeholder="back" value={title}></input>
+                            <input id="title" name="title"type="text" placeholder="back" value={title} onChange={this.handleChangeTitle}></input>
                           </div>
                           <div>
                             <label htmlFor="workout1">workout:</label>
                             <input id="workout1" name="workout1"type="text" placeholder="back extensions" 
-                            value={workout1}></input>
+                            value={workout1}
+                            onChange={this.handleChangeWorkout1}></input>
                           </div>
                           <div>
                             <label htmlFor="lbs">lbs:</label>
-                            <input id="lbs" name="lbs" type="number" placeholder="50lbs" value={lbs}></input>
+                            <input id="lbs" name="lbs" type="number" placeholder="50lbs" value={lbs} onChange={this.handleChangelbs}></input>
                           </div>
                           <div>
                               <label htmlFor="set1">set1:</label>
-                              <input id="set1" name="set1" type="number" placeholder="10reps" value={set1}></input>
+                              <input id="set1" name="set1" type="number" placeholder="10reps" value={set1} onChange={this.handleChangeSet1}></input>
                           </div>
                           <div>
                               <label htmlFor="set2">set2:</label>
-                              <input id="set2" name="set2" type="number" placeholder="10reps" value={set2}></input>
+                              <input id="set2" name="set2" type="number" placeholder="10reps" value={set2} onChange={this.handleChangeSet2}></input>
                           </div>
                           <div>
                               <label htmlFor="set3">set3:</label>
-                              <input id="set3" name="set3" type="number" placeholder="10reps" value={set3}></input>
+                              <input id="set3" name="set3" type="number" placeholder="10reps" value={set3} onChange={this.handleChangeSet3}></input>
                           </div>
 
 
