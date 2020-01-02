d39a06470b24c2071e541ebbf2e062f3b12d2c52
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-02 16:19:31 -0500

    added snapshot testing to workoutlists and workoutitem. tests are passing.

--- src/Components/WorkoutItem/WorkoutItem.test.js
+++ src/Components/WorkoutItem/WorkoutItem.test.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WorkoutList from '../WorkoutList/WorkoutList';
+import { BrowserRouter } from 'react-router-dom'
+import WorkoutItem from '../WorkoutItem/WorkoutItem';
 import renderer from 'react-test-renderer'
 
 
 describe('GET /workouts' , () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<WorkoutList />, div);
+    ReactDOM.render(<BrowserRouter><WorkoutItem /></BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('renders the UI as expected', () => {
     const tree = renderer
-      .create(<WorkoutList />)
+      .create(<BrowserRouter><WorkoutItem /></BrowserRouter>)
       .toJSON()
     expect(tree).toMatchSnapshot();
   })
- 
-
 })
