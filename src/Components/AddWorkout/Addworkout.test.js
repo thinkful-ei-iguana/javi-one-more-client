fd22638aab8e28456f33e811d3fc1aa7c89c9590
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-07 13:48:45 -0500

    fixed addworkout form from overflowing

--- src/Components/AddWorkout/Addworkout.test.js
+++ src/Components/AddWorkout/Addworkout.test.js
@@ -6,6 +6,8 @@ import renderer from 'react-test-renderer'
 
 
 describe('add workout' , () => {
+
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<BrowserRouter><Addworkout /></BrowserRouter>, div);
@@ -18,4 +20,6 @@ describe('add workout' , () => {
       .toJSON()
     expect(tree).toMatchSnapshot();
   })
+
+  
 })
