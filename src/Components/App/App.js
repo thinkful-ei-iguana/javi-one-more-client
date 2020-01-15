9bfd38c222ceb25cb2f7d4f86ce46914650fdad4
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-15 13:43:56 -0500

    fixed spelling errors

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,7 +9,7 @@ import WorkoutPage from '../WorkoutPage/WorkoutPage.js'
 import AddWorkout from '../AddWorkout/AddWorkout.js'
 import EditWorkout from '../EditWorkout/EditWorkout.js'
 import WorkoutInfo from '../WorkoutInfo/WorkoutInfo.js'
-import UserWorkoutPage from '../UsersWorkoutPage/UserWorkoutPage.js'
+import UserWorkoutPage from '../UserWorkoutPage/UserWorkoutPage.js'
 
 class App extends Component {
 
