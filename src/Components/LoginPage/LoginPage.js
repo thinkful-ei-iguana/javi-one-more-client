2f0e690bc3b48edcec8477b2d892697b53971a17
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-15 07:03:47 -0500

    changed prop name

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -21,7 +21,7 @@ class LoginPage extends Component {
         return (
             <div>
                 <LoginForm 
-                    history={this.handleLoginSuccess}
+                    onLoginSuccess={this.handleLoginSuccess}
                 />
             </div>
         )
